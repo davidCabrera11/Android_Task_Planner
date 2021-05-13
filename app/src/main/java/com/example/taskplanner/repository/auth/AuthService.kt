8fca2fc6dcfa1f0a6646755eaf81ab0a5fb3b2fd
David Cabrera <davidcabrera052196@gmail.com>
2021-05-12 22:10:44 -0500

    Created data class for token mapping

--- app/src/main/java/com/example/taskplanner/repository/auth/AuthService.kt
+++ app/src/main/java/com/example/taskplanner/repository/auth/AuthService.kt
@@ -1,11 +1,13 @@
 package com.example.taskplanner.repository.auth
 
+import com.example.taskplanner.repository.dto.LoginDto
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
 
     @POST("/auth")
-    suspend fun auth(){
+    suspend fun auth(@Body loginDto: LoginDto){
 
 
     }
---
+++ app/src/main/java/com/example/taskplanner/repository/dto/TokenDto.kt
@@ -0,0 +1,5 @@
+package com.example.taskplanner.repository.dto
+
+import java.util.*
+
+data class TokenDto(val token:String, val expirationTime: Date)
