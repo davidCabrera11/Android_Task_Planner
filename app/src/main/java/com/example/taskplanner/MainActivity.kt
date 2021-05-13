cdada726cd1f66ca8b8cc6737bdb8588712f7535
David Cabrera <davidcabrera052196@gmail.com>
2021-05-12 22:20:40 -0500

    Added auth function to main activity with coroutine scope

--- app/src/main/java/com/example/taskplanner/MainActivity.kt
+++ app/src/main/java/com/example/taskplanner/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.taskplanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.taskplanner.repository.RetrofitGenerator
+import com.example.taskplanner.repository.auth.AuthService
+import com.example.taskplanner.repository.dto.LoginDto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth()
+
     }
+
+    private fun auth() {
+        val authService = RetrofitGenerator.retrofit.create(AuthService::class.java)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            authService.auth(LoginDto("davidcab11@gmail.com","passw0rd"))
+
+
+        }
+
+     }
 }
--- app/src/main/java/com/example/taskplanner/repository/auth/AuthService.kt
+++ app/src/main/java/com/example/taskplanner/repository/auth/AuthService.kt
@@ -1,16 +1,17 @@
 package com.example.taskplanner.repository.auth
 
 import com.example.taskplanner.repository.dto.LoginDto
+import com.example.taskplanner.repository.dto.TokenDto
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
 
     @POST("/auth")
-    suspend fun auth(@Body loginDto: LoginDto){
+    suspend fun auth(@Body loginDto: LoginDto): Response<TokenDto>
 
 
-    }
 
 
 }
