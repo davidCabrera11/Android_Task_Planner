dcf55db036a820e1accf3d4ed0fb95e7f809a741
David Cabrera <davidcabrera052196@gmail.com>
2021-05-12 21:48:57 -0500

    Added retrofit builder and loggingInterceptor

--- app/src/main/java/com/example/taskplanner/repository/RetrofitGenerator.kt
+++ app/src/main/java/com/example/taskplanner/repository/RetrofitGenerator.kt
@@ -1,2 +1,38 @@
 package com.example.taskplanner.repository
 
+import com.example.taskplanner.BuildConfig
+import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitGenerator {
+
+
+    val retrofit: Retrofit by lazy{
+
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .writeTimeout(0,TimeUnit.MILLISECONDS)
+            .readTimeout(2, TimeUnit.MINUTES)
+            .connectTimeout(1, TimeUnit.MINUTES).build()
+
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.API_BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+        return@lazy retrofit
+
+
+
+    }
+
+}
