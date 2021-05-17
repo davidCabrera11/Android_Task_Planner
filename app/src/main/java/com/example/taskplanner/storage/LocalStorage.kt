ce8bcc674c0b21f42816d4d6135eb8c73418301b
David Cabrera <davidcabrera052196@gmail.com>
2021-05-17 11:52:36 -0500

    Added functions to interface and created new class

---
+++ app/src/main/java/com/example/taskplanner/storage/LocalStorage.kt
@@ -0,0 +1,16 @@
+package com.example.taskplanner.storage
+
+class LocalStorage : Storage {
+
+    override fun saveToken(token: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getToken(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun clear() {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/example/taskplanner/storage/Storage.kt
+++ app/src/main/java/com/example/taskplanner/storage/Storage.kt
@@ -1,4 +1,10 @@
 package com.example.taskplanner.storage
 
 interface Storage {
+
+    fun saveToken(token:String)
+
+    fun getToken(): String
+
+    fun clear()
 }
