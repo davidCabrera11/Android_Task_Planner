6852502a2688daff584fb02f4eda8f8e79c0edea
David Cabrera <davidcabrera052196@gmail.com>
2021-05-17 12:00:37 -0500

    Created kotlin file with constants that will be used

--- app/src/main/java/com/example/taskplanner/storage/LocalStorage.kt
+++ app/src/main/java/com/example/taskplanner/storage/LocalStorage.kt
@@ -1,6 +1,14 @@
 package com.example.taskplanner.storage
 
-class LocalStorage : Storage {
+import android.content.Context
+import android.provider.Settings.Global.getString
+import com.example.taskplanner.R
+import com.example.taskplanner.utils.SHARED_PREFERENCE_FILE_NAME
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+class LocalStorage(@ApplicationContext context: Context) : Storage {
+
+    val sharedPref = context.getSharedPreferences(SHARED_PREFERENCE_FILE_NAME, Context.MODE_PRIVATE)
 
     override fun saveToken(token: String) {
         TODO("Not yet implemented")
---
+++ app/src/main/java/com/example/taskplanner/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.taskplanner.utils
+
+const val SHARED_PREFERENCE_FILE_NAME = "my_shared_preferences"
+const val TOKEN_KEY = "token-key"
