c540b4ef4bdcc033729554b6eec7721a340198d9
David Cabrera <davidcabrera052196@gmail.com>
2021-05-17 20:47:19 -0500

    Implemented CRUD functions on interfaces task and user

--- app/src/main/java/com/example/taskplanner/repository/task/TaskService.kt
+++ app/src/main/java/com/example/taskplanner/repository/task/TaskService.kt
@@ -1,4 +1,28 @@
 package com.example.taskplanner.repository.task
 
+import com.example.taskplanner.repository.dto.TaskDto
+import com.example.taskplanner.repository.dto.UserDto
+import retrofit2.Response
+import retrofit2.http.*
+
 interface TaskService {
+
+
+    @POST
+    suspend fun createTask(@Body taskDto: TaskDto): Response<TaskDto>
+
+    @GET
+    suspend fun getTasks(): List<TaskDto>
+
+    @GET("tasks/{id}")
+    suspend fun findTaskById(@Path("id") id:String): Response<TaskDto>
+
+    @PUT("tasks/{id}")
+    suspend fun updateTask(@Path("id")id: String, @Body taskDto: TaskDto):Response<TaskDto>
+
+    @DELETE("tasks/{id}")
+    suspend fun deleteTask(@Path("id") id:String)
+
+
+
 }
--- app/src/main/java/com/example/taskplanner/repository/user/UserService.kt
+++ app/src/main/java/com/example/taskplanner/repository/user/UserService.kt
@@ -2,12 +2,24 @@ package com.example.taskplanner.repository.user
 
 import com.example.taskplanner.repository.dto.UserDto
 import retrofit2.Response
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface UserService {
 
+
+    @POST
+    suspend fun createUser(@Body userDto: UserDto): Response<UserDto>
+
+    @GET
+    suspend fun getUsers(): List<UserDto>
+
     @GET("users/{id}")
     suspend fun findUserById(@Path("id") id:String):Response<UserDto>
+
+    @PUT("users/{id}")
+    suspend fun updateUser(@Path("id")id: String, @Body userDto: UserDto):Response<UserDto>
+
+    @DELETE("users/{id}")
+    fun deleteUser(@Path("id")id:String)
 
 }
