dbc02fa0c8f014522cb6197960593f952bf3619c
David Cabrera <davidcabrera052196@gmail.com>
2021-05-27 14:37:42 -0500

    Added function to validate form

--- app/src/main/java/com/example/taskplanner/ui/main/LoginActivity.kt
+++ app/src/main/java/com/example/taskplanner/ui/main/LoginActivity.kt
@@ -2,6 +2,9 @@ package com.example.taskplanner.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.taskplanner.R
@@ -14,12 +17,24 @@ class LoginActivity: AppCompatActivity() {
 
     val viewModel by viewModels<LoginActivityViewModel>()
 
+    val button = findViewById<Button>(R.id.btnLogin)
+
+    val emailEditText = findViewById<EditText>(R.id.editTextEmail)
+
+    val passwordEditText = findViewById<EditText>(R.id.editTextPassword)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
 
+        button.setOnClickListener {
+            if (validFormField()){
+
+            }
+
+        }
 
         viewModel.successLiveData.observe(this, {success ->
             if (success){
@@ -30,9 +45,28 @@ class LoginActivity: AppCompatActivity() {
         //viewModel.auth()
     }
 
+    private fun validFormField(): Boolean {
+
+        if (emailEditText.text.isEmpty()){
+            emailEditText.error = getString(R.string.field_cannot_be_empty)
+            return false
+        }else if (android.util.Patterns.EMAIL_ADDRESS.matcher(emailEditText.text).matches()){
+            emailEditText.error = getString(R.string.invalid_email_address)
+            return false
+        }else{
+            emailEditText.error = null
+        }
+
+        if (passwordEditText.text.isEmpty()){
+            passwordEditText.error = getString(R.string.field_cannot_be_empty)
+            return false
+        }
 
 
+        return true
 
+
+    }
 
 
 }
