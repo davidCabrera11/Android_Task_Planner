9c1122cad0d2d155b134f95ea0736bc38cb1ac12
David Cabrera <davidcabrera052196@gmail.com>
2021-05-29 10:35:21 -0500

    changed order function call

--- app/src/main/java/com/example/taskplanner/ui/main/LoginActivity.kt
+++ app/src/main/java/com/example/taskplanner/ui/main/LoginActivity.kt
@@ -24,8 +24,8 @@ class LoginActivity: AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         btnLogin.setOnClickListener {
-            validFormField()
             viewModel.auth(editTextEmail.text.toString(),editTextPassword.text.toString())
+            validFormField()
 
         }
 
