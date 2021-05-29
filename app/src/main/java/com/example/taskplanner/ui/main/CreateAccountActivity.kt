539a1577b4ca632f5e0cb91c581eb2392da9efb1
David Cabrera <davidcabrera052196@gmail.com>
2021-05-28 23:37:04 -0500

    Created link to create account vm

--- app/src/main/java/com/example/taskplanner/ui/main/CreateAccountActivity.kt
+++ app/src/main/java/com/example/taskplanner/ui/main/CreateAccountActivity.kt
@@ -2,18 +2,24 @@ package com.example.taskplanner.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.taskplanner.R
+import com.example.taskplanner.viewmodel.CreateAccountViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_login.*
 
 @AndroidEntryPoint
 class CreateAccountActivity:AppCompatActivity() {
 
+    val viewModel by viewModels<CreateAccountViewModel>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+
 
 
     }
--- app/src/main/java/com/example/taskplanner/viewmodel/CreateAccountViewModel.kt
+++ app/src/main/java/com/example/taskplanner/viewmodel/CreateAccountViewModel.kt
@@ -1,9 +1,23 @@
 package com.example.taskplanner.viewmodel
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.taskplanner.repository.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class CreateAccountViewModel {
+class CreateAccountViewModel @Inject constructor(private val userRepository: UserRepository):ViewModel() {
+
+    val successLiveData = MutableLiveData<Boolean>()
+
+    fun createUser(){
+
+
+
+    }
+
+
 
 
 }
