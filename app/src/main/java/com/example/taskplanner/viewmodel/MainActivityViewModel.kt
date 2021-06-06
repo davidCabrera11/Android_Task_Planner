a427be381ebf6f2d5b01d127664924b2316b1875
David Cabrera <davidcabrera052196@gmail.com>
2021-06-05 20:23:03 -0500

    Created fragment to create new tasks

--- app/src/main/java/com/example/taskplanner/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/taskplanner/viewmodel/MainActivityViewModel.kt
@@ -48,60 +48,11 @@ class MainActivityViewModel @Inject constructor(
 
     }
 
-    fun updateUser() {
-        GlobalScope.launch(Dispatchers.IO) {
-            val response = userRepository.userService.updateUser("6099af1ab755fe29152c9272",
-                UserDto("","Itachi","securePassword123","jorge123@gmail.com","imgur.com"))
-            if(response.isSuccessful){
-                val user = response.body()!!
-                Log.d("DEBUG", "Updated UserId's : $user")
-                successLiveData.postValue(true)
-                userRepository.userDao.save(User(user))
 
-            }else{
-                response.errorBody()
-                successLiveData.postValue(false)
 
-            }
 
-        }
-    }
 
-    fun findTaskById(){
-        viewModelScope.launch(Dispatchers.IO) {
-            val response = taskRepository.taskService.findTaskById("60a3326448abc1562e500144")
-            if(response.isSuccessful){
-                val task = response.body()!!
-                Log.d("DEBUG", "Find task by id: $task")
-                successLiveData.postValue(true)
-                taskRepository.taskDao.save(Task(task))
-            }else{
-                response.errorBody()
-                successLiveData.postValue(false)
-            }
-        }
-    }
 
-    fun updateTask() {
-        GlobalScope.launch(Dispatchers.IO) {
-            val response = taskRepository.taskService.updateTask("60a3326448abc1562e500144",
-                TaskDto("3123123","Complete house chores","Test",
-                    "2021-05-19T03:18:42.507+00:00","To-do","123123123")
-            )
-            if(response.isSuccessful){
-                val task = response.body()!!
-                Log.d("DEBUG", "Updated task: $task")
-                successLiveData.postValue(true)
-                taskRepository.taskDao.save(Task(task))
-
-            }else{
-                response.errorBody()
-                successLiveData.postValue(false)
-
-            }
-
-        }
-    }
 
 
 
