c71afb19c83b27779282ae3b4b46ce6898dcd6fa
David Cabrera <davidcabrera052196@gmail.com>
2021-06-06 10:18:04 -0500

    Created fragment for recyclerview

---
+++ app/src/main/java/com/example/taskplanner/ui/fragments/RecycleViewFragment.kt
@@ -0,0 +1,23 @@
+package com.example.taskplanner.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskplanner.R
+
+
+class RecycleViewFragment : Fragment(R.layout.fragment_recycle_view) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+
+
+
+}
