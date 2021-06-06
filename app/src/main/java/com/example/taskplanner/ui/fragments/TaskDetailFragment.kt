3f89242f3f0aaac5ed05753c252e322cc6c2dffd
David Cabrera <davidcabrera052196@gmail.com>
2021-06-06 10:43:56 -0500

    Created fragment for updating task and user profile

---
+++ app/src/main/java/com/example/taskplanner/ui/fragments/TaskDetailFragment.kt
@@ -0,0 +1,21 @@
+package com.example.taskplanner.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskplanner.R
+
+
+
+class TaskDetailFragment : Fragment(R.layout.fragment_task_detail,) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/taskplanner/ui/fragments/UpdateProfileFragment.kt
@@ -0,0 +1,19 @@
+package com.example.taskplanner.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskplanner.R
+
+
+
+class UpdateProfileFragment : Fragment(R.layout.fragment_update_profile) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
