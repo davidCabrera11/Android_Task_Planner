2d4b0aac46fb96257e14481e7ea554f3d67aac93
David Cabrera <davidcabrera052196@gmail.com>
2021-06-07 18:26:23 -0500

    Updated task detail fragment layout

--- app/src/main/java/com/example/taskplanner/ui/fragments/NewTaskFragment.kt
+++ app/src/main/java/com/example/taskplanner/ui/fragments/NewTaskFragment.kt
@@ -2,22 +2,13 @@ package com.example.taskplanner.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import androidx.activity.viewModels
 import androidx.fragment.app.viewModels
 import com.example.taskplanner.R
-import com.example.taskplanner.repository.remote.dto.TaskDto
-import com.example.taskplanner.repository.remote.task.TaskService
 import com.example.taskplanner.viewmodel.MainActivityViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class NewTaskFragment : Fragment(R.layout.fragment_new_task) {
@@ -29,11 +20,11 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val buttonCreateTask = view.findViewById<Button>(R.id.buttonCreateTask)
-        val description = view.findViewById<EditText>(R.id.editTextDescriptionTask)
-        val personResponsable = view.findViewById<EditText>(R.id.editTextPersonResponsible)
-        val dueDate = view.findViewById<EditText>(R.id.editTextDueDate)
-        val status = view.findViewById<EditText>(R.id.editTextStatus)
+        val buttonCreateTask = view.findViewById<Button>(R.id.buttonUpdateTask)
+        val description = view.findViewById<EditText>(R.id.textViewUpdateDescriptionTask)
+        val personResponsable = view.findViewById<EditText>(R.id.textViewUpdatePersonResponsible)
+        val dueDate = view.findViewById<EditText>(R.id.textViewUpdateDueDate)
+        val status = view.findViewById<EditText>(R.id.textViewUpdateStatus)
 
 
 
