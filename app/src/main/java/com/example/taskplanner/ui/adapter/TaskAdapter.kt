4b416a311582f8f93d4b2f8c5e2a1d9c075ffeba
David Cabrera <davidcabrera052196@gmail.com>
2021-06-12 00:21:39 -0500

    fixed date format

--- app/src/main/java/com/example/taskplanner/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/taskplanner/ui/adapter/TaskAdapter.kt
@@ -1,14 +1,18 @@
 package com.example.taskplanner.ui.adapter
 
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.taskplanner.R
 import com.example.taskplanner.repository.remote.dto.TaskDto
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class TaskAdapter(private val taskAdapterListener: TaskAdapterListener): RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
 
@@ -17,7 +21,7 @@ class TaskAdapter(private val taskAdapterListener: TaskAdapterListener): Recycle
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val description: TextView = view.findViewById(R.id.textViewTaskDescription)
-        val dueDate: TextView = view.findViewById(R.id.textViewTaskDueDate)
+        var dueDate: TextView = view.findViewById(R.id.textViewTaskDueDate)
         val status: TextView = view.findViewById(R.id.textViewTaskStatus)
 
     }
@@ -32,7 +36,7 @@ class TaskAdapter(private val taskAdapterListener: TaskAdapterListener): Recycle
         val task = taskList[position]
 
         holder.description.text = task.description
-        holder.dueDate.text = task.dueDate
+        holder.dueDate.text = task.dueDate.subSequence(0,10)
         holder.status.text = task.status
 
 
